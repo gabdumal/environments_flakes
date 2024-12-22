c36f35dfd43065df3682368397ef7cb9651de5db
Gabriel Malosto <professional@gabdumal.com>
2024-12-22 00:49:26 -0300

    Create environment for Python

---
+++ python/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A Flake for a Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell.override
+          { }
+          {
+            packages = with pkgs; [
+              python312
+            ] ++
+            (with pkgs.python312Packages; [
+              pip
+              ruff
+              venvShellHook
+            ]);
+            venvDir = ".venv";
+          };
+      });
+    };
+}
